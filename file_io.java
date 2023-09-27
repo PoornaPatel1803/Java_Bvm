cb736f504326d1a4ee5b0bf21decca6fd66e02c9
Poorna Patel <139461234+PoornaPatel1803@users.noreply.github.com>
2023-09-27 12:28:28 +0530

    Add files via upload

---
+++ file_io.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class file_io {
+    public static void main(String args[])
+            throws IOException {
+        int i;
+        FileInputStream fin;
+        FileOutputStream fout;
+        try {
+            // open input file
+            try {
+                fin = new FileInputStream("copy.txt");
+            } catch (FileNotFoundException e) {
+                System.out.println("Input File Not Found");
+                return;
+            }
+            // open output file
+            try {
+                fout = new FileOutputStream("copied.txt");
+            } catch (FileNotFoundException e) {
+                System.out.println("Error Opening Output File");
+                return;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: CopyFile From To");
+            return;
+        }
+        // Copy File
+        try {
+            do {
+                i = fin.read();
+                if (i != -1)
+                    fout.write(i);
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println("File Error");
+        }
+        fin.close();
+        fout.close();
+    }
+}
